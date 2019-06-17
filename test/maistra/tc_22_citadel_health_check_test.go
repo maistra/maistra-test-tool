35bab391a2cbffd8de341beafe91533e87719cd4
yxun <yuanlin.yxu@gmail.com>
2019-06-17 16:33:22 -0400

    Fixed tc 22 wrong cleanup and fixed issue when running test before bookinfo ns creation

--- test/maistra/tc_22_citadel_health_check_test.go
+++ test/maistra/tc_22_citadel_health_check_test.go
@@ -29,12 +29,11 @@ import (
 func cleanup22(kubeconfig string) {
 	log.Infof("# Cleanup. Following error can be ignored...")
 	util.Shell("kubectl rollout undo deployment -n istio-system istio-citadel")
-	//util.ShellMuteOutput("rm -f /tmp/istio-citadel-new.yaml")
-	util.ShellMuteOutput("kubectl delete service istio-citadel -n istio-system")
-	util.ShellMuteOutput("kubectl delete serviceaccount istio-citadel-service-account -n istio-system")
+	util.ShellMuteOutput("rm -f /tmp/istio-citadel-new.yaml")
+
 	util.ShellMuteOutput("kubectl delete meshpolicy default")
-	log.Info("Waiting... Sleep 10 seconds...")
-	time.Sleep(time.Duration(10) * time.Second)
+	log.Info("Waiting... Sleep 20 seconds...")
+	time.Sleep(time.Duration(20) * time.Second)
 }
 
 
@@ -47,51 +46,57 @@ func Test22(t *testing.T) {
 		}
 	}()
 
-	log.Info("# TC_22 Citadel Health Checking")
-	log.Info("Enable mTLS")
-	util.Inspect(util.KubeApplyContents("", meshPolicy, kubeconfigFile), "failed to apply MeshPolicy", "", t)
-	log.Info("Waiting... Sleep 5 seconds...")
-	time.Sleep(time.Duration(5) * time.Second)	
+	t.Run("citadel_health_check_test", func(t *testing.T) {
+		defer func() {
+			// recover from panic if one occured. This allows cleanup to be executed after panic.
+			if err := recover(); err != nil {
+				t.Errorf("Test panic: %v", err)
+			}
+		}()
 
-	log.Info("Redeploy Citadel")
-	backupFile := "/tmp/istio-citadel-bak.yaml"
-	newFile := "/tmp/istio-citadel-new.yaml"
-
-	util.ShellMuteOutput("kubectl get deployment -n %s %s -o yaml --kubeconfig=%s > %s",
+		log.Info("# TC_22 Citadel Health Checking")
+		log.Info("Enable mTLS")
+		util.Inspect(util.KubeApplyContents("", meshPolicy, kubeconfigFile), "failed to apply MeshPolicy", "", t)
+		log.Info("Waiting... Sleep 5 seconds...")
+		time.Sleep(time.Duration(5) * time.Second)	
+		
+		log.Info("Redeploy Citadel")
+		backupFile := "/tmp/istio-citadel-bak.yaml"
+		newFile := "/tmp/istio-citadel-new.yaml"
+		
+		util.ShellMuteOutput("kubectl get deployment -n %s %s -o yaml --kubeconfig=%s > %s",
 						"istio-system",
 						"istio-citadel",
 						kubeconfigFile,
 						backupFile)
 
-	data, err := ioutil.ReadFile(backupFile)
-	if err != nil {
-		log.Infof("Unable to read citadel deployment yaml: %v", err)
-		t.Errorf("Unable to read citadel deployment yaml: %v", err)
-	}
-	w, _ := os.Create(newFile)
-	defer w.Close()
+		data, err := ioutil.ReadFile(backupFile)
+		if err != nil {
+			log.Infof("Unable to read citadel deployment yaml: %v", err)
+			t.Errorf("Unable to read citadel deployment yaml: %v", err)
+		}
+		w, _ := os.Create(newFile)
+		defer w.Close()
+		
+		err = util.ConfigCitadelHealthCheck(data, w)
+		if err != nil {
+			log.Infof("Update citadel deployment error: %v", err)
+			t.Errorf("Update citadel deployment error: %v", err)
+		}
+		util.Shell("kubectl apply -n %s -f %s", "istio-system", newFile)
+		log.Info("Waiting... Sleep 30 seconds...")
+		time.Sleep(time.Duration(30) * time.Second)
 
-	util.Inspect(util.KubeApplyContents("", citadelServiceAccount, kubeconfigFile), "failed to apply citadel service account", "", t)
-	util.Inspect(util.KubeApplyContents("", citadelService, kubeconfigFile), "failed to apply citadel service account", "", t)
-	
-	err = util.ConfigCitadelHealthCheck(data, w)
-	if err != nil {
-		log.Infof("Update citadel deployment error: %v", err)
-		t.Errorf("Update citadel deployment error: %v", err)
-	}
-	util.Shell("kubectl apply -n %s -f %s", "istio-system", newFile)
-	log.Info("Waiting... Sleep 30 seconds...")
-	time.Sleep(time.Duration(30) * time.Second)
-
-	pod, err := util.GetPodName("istio-system", "istio=citadel", kubeconfigFile)
-	util.Inspect(err, "failed to get sleep pod name", "", t)
-	msg, _ := util.ShellMuteOutput("kubectl logs -n %s %s", "istio-system", pod)
-	if !strings.Contains(msg, "CSR") {
-		log.Infof("Error no CSR is healthy log")
-		t.Errorf("Error no CSR is healthy log")
-	} else {
-		re, _ := regexp.Compile(".*CSR.*")
-		log.Infof("%v", re.FindString(msg))
-	}
+		pod, err := util.GetPodName("istio-system", "istio=citadel", kubeconfigFile)
+		util.Inspect(err, "failed to get sleep pod name", "", t)
+		msg, _ := util.ShellMuteOutput("kubectl logs -n %s %s", "istio-system", pod)
+		if !strings.Contains(msg, "CSR") {
+			log.Infof("Error no CSR is healthy log")
+			t.Errorf("Error no CSR is healthy log")
+		} else {
+			re, _ := regexp.Compile(".*CSR.*")
+			log.Infof("%v", re.FindString(msg))
+		}
+	})
 
 }
