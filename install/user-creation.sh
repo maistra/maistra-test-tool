4936882a0c58ebeb2e61c959b6faf4712c0fb390
yxun <yuanlin.yxu@gmail.com>
2019-08-21 10:21:11 -0400

    Remove dev users in maistra-1.0 branch

--- install/user-creation.sh
+++ install/user-creation.sh
@@ -5,9 +5,6 @@
 function create_htpasswd_file() {
   htpasswd -c -B -b users.htpasswd qe1 "${QE1_PWD:-qe1pw}"
   htpasswd -B -b users.htpasswd qe2 "${QE2_PWD:-qe2pw}"
-  htpasswd -B -b users.htpasswd ike "${IKE_PWD:-let_ike_in}"
-  htpasswd -B -b users.htpasswd aslak "${ASLAK_PWD:-let_aslak_in}"
-  htpasswd -B -b users.htpasswd bartosz "${BARTOSZ_PWD:-let_bartosz_in}"
 }
 
 function create_htpasswd_secret() {
@@ -32,18 +29,11 @@ EOF
 )
 }
 
-function create_cluster_admin_user() {
-  oc adm policy add-cluster-role-to-user cluster-admin "ike"
-  oc adm policy add-cluster-role-to-user cluster-admin "aslak"
-  oc adm policy add-cluster-role-to-user cluster-admin "bartosz"
-
-}
 
 function main() {
   create_htpasswd_file
   create_htpasswd_secret
   update_oauth
-  create_cluster_admin_user
 }
 
 main
