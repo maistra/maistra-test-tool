0f756f62d9d1cac59592d459cb3a66619e7e87e3
yxun <yuanlin.yxu@gmail.com>
2020-04-14 11:36:13 -0400

    Add auth mtls deep-dive test and update case numbers

--- test_cases.go
+++ test_cases.go
@@ -19,65 +19,69 @@ import "testing"
 var t = &testing.T{}
 var testCases = []testing.InternalTest{
 	testing.InternalTest{
-		Name: "03",
+		Name: "01",
 		F:    TestRequestRouting,
 	},
 	testing.InternalTest{
-		Name: "04",
+		Name: "02",
 		F:    TestFaultInjection,
 	},
 	testing.InternalTest{
-		Name: "05",
+		Name: "03",
 		F:    TestTrafficShifting,
 	},
 	/*
 		testing.InternalTest {
-			Name: "06",
+			Name: "04",
 			F: TestTCPShifting,
 		},
 	*/
 	testing.InternalTest{
-		Name: "07",
+		Name: "05",
 		F:    TestRequestTimeouts,
 	},
 	testing.InternalTest{
-		Name: "08",
+		Name: "06",
 		F:    TestCircuitBreaking,
 	},
 	testing.InternalTest{
-		Name: "09",
+		Name: "07",
 		F:    TestMirroring,
 	},
 	testing.InternalTest{
-		Name: "10",
+		Name: "08",
 		F:    TestIngressGateways,
 	},
 	testing.InternalTest{
-		Name: "11",
+		Name: "09",
 		F:    TestIngressGatewaysFileMount,
 	},
 	testing.InternalTest{
-		Name: "12",
+		Name: "10",
 		F:    TestIngressWithOutTLS,
 	},
 	testing.InternalTest{
-		Name: "13",
+		Name: "11",
 		F:    TestAccessExternalServices,
 	},
 	testing.InternalTest{
-		Name: "14",
+		Name: "12",
 		F:    TestEgressTLSOrigination,
 	},
 	testing.InternalTest{
-		Name: "15",
+		Name: "13",
 		F:    TestEgressGateways,
 	},
 	testing.InternalTest{
-		Name: "16",
+		Name: "14",
 		F:    TestEgressGatewaysTLSOrigination,
 	},
 	testing.InternalTest{
-		Name: "17",
+		Name: "15",
 		F:    TestAuthPolicy,
+	},
+	testing.InternalTest{
+		Name: "16",
+		F:    TestAuthMTLS,
 	},
 }
