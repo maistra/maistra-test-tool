a8a844dbfdb702e2a2b4e963a13eee57a632031d
yxun <yuanlin.yxu@gmail.com>
2020-11-04 14:23:34 -0500

    Add test namespaces creation steps

--- tests/main.sh
+++ tests/main.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+oc new-project bookinfo
+oc new-project foo
+oc new-project bar
+oc new-project legacy
+
 oc adm policy add-scc-to-user anyuid -z default -n bookinfo
 oc adm policy add-scc-to-user anyuid -z bookinfo-ratings-v2 -n bookinfo
 oc adm policy add-scc-to-user anyuid -z httpbin -n bookinfo
