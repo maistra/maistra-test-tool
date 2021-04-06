e3f6b1fca230cba085d0e7e1b565d06ae8a290d5
yxun <yuanlin.yxu@gmail.com>
2021-04-06 15:31:30 -0400

    Add a traffic delay scale test

---
+++ scripts/clean_members_200.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cat << EOF > ServiceMeshMemberRoll.yaml
+apiVersion: maistra.io/v1
+kind: ServiceMeshMemberRoll
+metadata:
+  name: default
+  namespace: istio-system
+spec:
+  members:
+    - bookinfo
+EOF
+
+oc apply -f ServiceMeshMemberRoll.yaml -n istio-system
+
+for i in {1..200}
+do
+  oc delete project test$i
+done
