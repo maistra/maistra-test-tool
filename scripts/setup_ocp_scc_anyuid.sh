c2a3e1b25f2150e0e65585f47ffe94b81e412af3
rashmi-redhat <64413893+rashmi-redhat@users.noreply.github.com>
2021-04-27 16:08:00 +0530

    Update setup_ocp_scc_anyuid.sh

--- scripts/setup_ocp_scc_anyuid.sh
+++ scripts/setup_ocp_scc_anyuid.sh
@@ -4,6 +4,7 @@ oc new-project bookinfo
 oc new-project foo
 oc new-project bar
 oc new-project legacy
+oc new-project exclude-outboundports-annotation
 
 oc adm policy add-scc-to-user anyuid -z default -n bookinfo
 oc adm policy add-scc-to-user anyuid -z bookinfo-ratings-v2 -n bookinfo
@@ -11,4 +12,6 @@ oc adm policy add-scc-to-user anyuid -z httpbin -n bookinfo
 oc adm policy add-scc-to-user anyuid -z httpbin -n foo
 oc adm policy add-scc-to-user anyuid -z httpbin -n bar
 oc adm policy add-scc-to-user anyuid -z httpbin -n legacy
+oc adm policy add-scc-to-user anyuid -z default -n exclude-outboundports-annotation
+oc adm policy add-scc-to-user anyuid -z httpbin -n exclude-outboundports-annotation
 sleep 5
