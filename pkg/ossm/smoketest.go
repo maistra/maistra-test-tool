cd3b720f83aaeea165dbadc986e2eaa88710e60f
yxun <yuanlin.yxu@gmail.com>
2022-03-07 10:30:36 -0500

    Update a smoketest route value

--- pkg/ossm/smoketest.go
+++ pkg/ossm/smoketest.go
@@ -55,12 +55,12 @@ func TestBookinfo(t *testing.T) {
 	}
 
 	util.Log.Info("Check if bookinfo productpage is running")
-	util.Shell(`export GATEWAY_URL=$(oc -n %s get route istio-ingressgateway -o jsonpath='{.spec.host}')`, meshNamespace)
-	mes, _ := util.Shell(`curl -o /dev/null -s -w "%%{http_code}\n" http://${GATEWAY_URL}/productpage`)
+	GATEWAY_URL, _ := util.Shell(`oc -n %s get route istio-ingressgateway -o jsonpath='{.spec.host}'`, meshNamespace)
+	mes, _ := util.Shell(`curl -o /dev/null -s -w "%%{http_code}\n" http://%s/productpage`, GATEWAY_URL)
 	if strings.Contains(mes, "200") {
 		util.Log.Info("Success. bookinfo productpage is running")
 	} else {
 		t.Error("Error. bookinfo productpage is not running.")
-
+		util.Log.Error("Error. bookinfo productpage is not running.")
 	}
 }
