92b6a6314524e90091dd068f275ce834ebff2cb8
prachiyadav <pyadav@redhat.com>
2022-03-24 23:06:04 +0530

    added smcp22 installation branch

--- pkg/ossm/yaml_configs.go
+++ pkg/ossm/yaml_configs.go
@@ -15,6 +15,33 @@
 package ossm
 
 const (
+  smcpV22_template = `
+apiVersion: maistra.io/v2
+kind: ServiceMeshControlPlane
+metadata:
+  name: {{ .Name }}
+spec:
+  version: v2.2
+  tracing:
+    type: Jaeger
+    sampling: 10000
+  policy:
+    type: Istiod
+  addons:
+    grafana:
+      enabled: true
+    jaeger:
+      install:
+        storage:
+          type: Memory
+    kiali:
+      enabled: true
+    prometheus:
+      enabled: true
+  
+  telemetry:
+    type: Istiod
+`
 	smcpV21_template = `
 apiVersion: maistra.io/v2
 kind: ServiceMeshControlPlane
