19567078966c9037c66590411b6c8142435f7d23
Filip Brychta <fbrychta@redhat.com>
2022-06-01 17:27:03 +0200

    OSSM-1461: do not use password parameter Password parameter is causing https://issues.jenkins.io/browse/JENKINS-68657 We need to use token in a string parameter as a workaround

--- scripts/pipeline/run_one_test.sh
+++ scripts/pipeline/run_one_test.sh
@@ -2,7 +2,12 @@
 
 go get -u github.com/jstemmer/go-junit-report
 
-oc login -u ${OCP_CRED_USR} -p ${OCP_CRED_PSW} --server=${OCP_API_URL} --insecure-skip-tls-verify=true
+if [ -z "${OCP_CRED_PSW}" ]
+then
+  oc login --token=${OCP_TOKEN} --server=${OCP_API_URL} --insecure-skip-tls-verify=true
+else
+  oc login -u ${OCP_CRED_USR} -p ${OCP_CRED_PSW} --server=${OCP_API_URL} --insecure-skip-tls-verify=true
+fi
 go test -timeout 3h -run ${TEST_CASE} -v 2>&1 | tee >($HOME/go/bin/go-junit-report > results.xml) test.log
 
 echo "#Testing Completed#"
