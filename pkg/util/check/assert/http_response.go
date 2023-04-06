922afa72bef4e531b6d8d6d87d0d6a5a71c50ce5
Marko Lukša <marko.luksa@gmail.com>
2023-04-06 23:06:27 +0200

    Ensure multiple asserts that read the response can be used in one curl.Request()

--- pkg/util/check/assert/http_response.go
+++ pkg/util/check/assert/http_response.go
@@ -10,43 +10,43 @@ import (
 )
 
 func ResponseMatchesFile(file string, successMsg, failureMsg string, otherFiles ...string) curl.HTTPResponseCheckFunc {
-	return func(t test.TestHelper, resp *http.Response, duration time.Duration) {
+	return func(t test.TestHelper, resp *http.Response, responseBody []byte, duration time.Duration) {
 		t.T().Helper()
-		common.CheckResponseMatchesFile(t, resp, file, successMsg, failureMsg, assertFailure, otherFiles...)
+		common.CheckResponseMatchesFile(t, resp, responseBody, file, successMsg, failureMsg, assertFailure, otherFiles...)
 	}
 }
 
 func ResponseStatus(expectedStatus int) curl.HTTPResponseCheckFunc {
-	return func(t test.TestHelper, resp *http.Response, duration time.Duration) {
+	return func(t test.TestHelper, resp *http.Response, responseBody []byte, duration time.Duration) {
 		t.T().Helper()
 		common.CheckResponseStatus(t, resp, expectedStatus, assertFailure)
 	}
 }
 
 func ResponseContains(str string) curl.HTTPResponseCheckFunc {
-	return func(t test.TestHelper, resp *http.Response, duration time.Duration) {
+	return func(t test.TestHelper, resp *http.Response, responseBody []byte, duration time.Duration) {
 		t.T().Helper()
-		common.CheckResponseContains(t, resp, str, assertFailure)
+		common.CheckResponseContains(t, resp, responseBody, str, assertFailure)
 	}
 }
 
 func ResponseDoesNotContain(str string) curl.HTTPResponseCheckFunc {
-	return func(t test.TestHelper, resp *http.Response, duration time.Duration) {
+	return func(t test.TestHelper, resp *http.Response, responseBody []byte, duration time.Duration) {
 		t.T().Helper()
-		common.CheckResponseDoesNotContain(t, resp, str, assertFailure)
+		common.CheckResponseDoesNotContain(t, resp, responseBody, str, assertFailure)
 	}
 }
 
 func DurationInRange(minDuration, maxDuration time.Duration) curl.HTTPResponseCheckFunc {
-	return func(t test.TestHelper, resp *http.Response, duration time.Duration) {
+	return func(t test.TestHelper, resp *http.Response, responseBody []byte, duration time.Duration) {
 		t.T().Helper()
 		common.CheckDurationInRange(t, resp, duration, minDuration, maxDuration, assertFailure)
 	}
 }
 
 func RequestFails(successMsg, failureMsg string) curl.HTTPResponseCheckFunc {
-	return func(t test.TestHelper, resp *http.Response, duration time.Duration) {
+	return func(t test.TestHelper, resp *http.Response, responseBody []byte, duration time.Duration) {
 		t.T().Helper()
-		common.CheckRequestFails(t, resp, successMsg, failureMsg, assertFailure)
+		common.CheckRequestFails(t, resp, responseBody, successMsg, failureMsg, assertFailure)
 	}
 }
--- pkg/util/check/require/http_response.go
+++ pkg/util/check/require/http_response.go
@@ -10,36 +10,36 @@ import (
 )
 
 func ResponseMatchesFile(file string, successMsg, failureMsg string, otherFiles ...string) curl.HTTPResponseCheckFunc {
-	return func(t test.TestHelper, resp *http.Response, duration time.Duration) {
+	return func(t test.TestHelper, resp *http.Response, responseBody []byte, duration time.Duration) {
 		t.T().Helper()
-		common.CheckResponseMatchesFile(t, resp, file, successMsg, failureMsg, requireFailure, otherFiles...)
+		common.CheckResponseMatchesFile(t, resp, responseBody, file, successMsg, failureMsg, requireFailure, otherFiles...)
 	}
 }
 
 func ResponseStatus(expectedStatus int) curl.HTTPResponseCheckFunc {
-	return func(t test.TestHelper, resp *http.Response, duration time.Duration) {
+	return func(t test.TestHelper, resp *http.Response, responseBody []byte, duration time.Duration) {
 		t.T().Helper()
 		common.CheckResponseStatus(t, resp, expectedStatus, requireFailure)
 	}
 }
 
 func ResponseContains(str string) curl.HTTPResponseCheckFunc {
-	return func(t test.TestHelper, resp *http.Response, duration time.Duration) {
+	return func(t test.TestHelper, resp *http.Response, responseBody []byte, duration time.Duration) {
 		t.T().Helper()
-		common.CheckResponseContains(t, resp, str, requireFailure)
+		common.CheckResponseContains(t, resp, responseBody, str, requireFailure)
 	}
 }
 
 func DurationInRange(minDuration, maxDuration time.Duration) curl.HTTPResponseCheckFunc {
-	return func(t test.TestHelper, resp *http.Response, duration time.Duration) {
+	return func(t test.TestHelper, resp *http.Response, responseBody []byte, duration time.Duration) {
 		t.T().Helper()
 		common.CheckDurationInRange(t, resp, duration, minDuration, maxDuration, requireFailure)
 	}
 }
 
 func RequestFails(successMsg, failureMsg string) curl.HTTPResponseCheckFunc {
-	return func(t test.TestHelper, resp *http.Response, duration time.Duration) {
+	return func(t test.TestHelper, resp *http.Response, responseBody []byte, duration time.Duration) {
 		t.T().Helper()
-		common.CheckRequestFails(t, resp, successMsg, failureMsg, requireFailure)
+		common.CheckRequestFails(t, resp, responseBody, successMsg, failureMsg, requireFailure)
 	}
 }
