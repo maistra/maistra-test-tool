b9bdab327b6f9f318ceca40533e744e57f6865ba
Mayleigh M <98288958+mayleighnmyers@users.noreply.github.com>
2023-04-10 13:04:32 -0400

    OSSM-3707 Refactor TestTLSVersionSMCP (T26), OSSM-3740 Refactor TestSSL (T27) (#419)
    * OSSM-3707 Refactor TestTLSVersionSMCP (T26)
    
    * Fix syntax errors and finish test
    
    * Made changes
    
    * Added new file for (T27)
    
    * Changed min to max
    
    * Changed a few errors
    
    ---------
    
    Co-authored-by: Marko Lukša <marko.luksa@gmail.com>

--- pkg/tests/ossm/smcp_tls_ssl_test.go
+++ pkg/tests/ossm/smcp_tls_ssl_test.go
@@ -4,152 +4,51 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 package ossm
 
 import (
-	_ "embed"
-	"strings"
 	"testing"
 
-	examples "github.com/maistra/maistra-test-tool/pkg/examples"
-	"github.com/maistra/maistra-test-tool/pkg/util"
-	"github.com/maistra/maistra-test-tool/pkg/util/env"
-	"github.com/maistra/maistra-test-tool/pkg/util/log"
-	"github.com/maistra/maistra-test-tool/pkg/util/test"
+	"github.com/maistra/maistra-test-tool/pkg/util/hack"
+	"github.com/maistra/maistra-test-tool/pkg/util/oc"
+	. "github.com/maistra/maistra-test-tool/pkg/util/test"
 )
-
-var (
-	//go:embed yaml/deployment-testssl-x86.yaml
-	testSSLDeployment string
-
-	//go:embed yaml/deployment-testssl-z.yaml
-	testSSLDeploymentZ string
-
-	//go:embed yaml/deployment-testssl-p.yaml
-	testSSLDeploymentP string
-)
-
-func cleanupTestTLSVersionSMCP() {
-	log.Log.Info("Cleanup ...")
-	util.Shell(`kubectl patch -n %s smcp/%s --type=json -p='[{"op": "remove", "path": "/spec/security/controlPlane/tls"}]'`, meshNamespace, smcpName)
-	util.Shell(`oc wait --for condition=Ready -n %s smcp/%s --timeout 180s`, meshNamespace, smcpName)
-}
-
-func cleanupTestSSL() {
-	log.Log.Info("Cleanup ...")
-	bookinfo := examples.Bookinfo{Namespace: "bookinfo"}
-	util.KubeDeleteContents("bookinfo", testSSLDeployment)
-	bookinfo.Uninstall()
-
-	util.Shell(`kubectl patch -n %s smcp/%s --type=json -p='[{"op": "remove", "path": "/spec/security/controlPlane/tls"}]'`, meshNamespace, smcpName)
-	util.Shell(`kubectl patch -n %s smcp/%s --type merge -p '{"spec":{"security":{"dataPlane":{"mtls":false},"controlPlane":{"mtls":false}}}}'`, meshNamespace, smcpName)
-	util.Shell(`oc wait --for condition=Ready -n %s smcp/%s --timeout 180s`, meshNamespace, smcpName)
-}
 
 func TestTLSVersionSMCP(t *testing.T) {
-	test.NewTest(t).Id("T26").Groups(test.Full, test.ARM, test.InterOp).NotRefactoredYet()
+	NewTest(t).Id("T26").Groups(Full, ARM, InterOp).Run(func(t TestHelper) {
+		hack.DisableLogrusForThisTest(t)
+		t.Log("This test checks if the SMCP updated the tls.minProtocolVersion to TLSv1_0, TLSv1_1, and tls.maxProtocolVersion to TLSv1_3.")
+		t.Cleanup(func() {
+			oc.Patch(t, meshNamespace,
+				"smcp", smcpName,
+				"json",
+				`[{"op": "remove", "path": "/spec/security/controlPlane/tls"}]`)
+			oc.WaitSMCPReady(t, meshNamespace, smcpName)
+		})
 
-	defer cleanupTestTLSVersionSMCP()
+		t.NewSubTest("minVersion_TLSv1_0").Run(func(t TestHelper) {
+			t.LogStep("Update SMCP spec.security.controlPlane.tls.minProtocolVersion: TLSv1_0")
+			oc.Patch(t, meshNamespace, "smcp", smcpName, "merge", `{"spec":{"security":{"controlPlane":{"tls":{"minProtocolVersion":"TLSv1_0"}}}}}`)
+			oc.WaitSMCPReady(t, meshNamespace, smcpName)
+		})
 
-	t.Run("Operator_test_smcp_global_tls_minVersion_TLSv1_0", func(t *testing.T) {
-		defer util.RecoverPanic(t)
+		t.NewSubTest("minVersion_TLSv1_1").Run(func(t TestHelper) {
+			t.LogStep("Check to see if the SMCP minProtocolVersion is TLSv1_1")
+			oc.Patch(t, meshNamespace, "smcp", smcpName, "merge", `{"spec":{"security":{"controlPlane":{"tls":{"minProtocolVersion":"TLSv1_1"}}}}}`)
+			oc.WaitSMCPReady(t, meshNamespace, smcpName)
+		})
 
-		log.Log.Info("Update SMCP spec.security.controlPlane.tls.minProtocolVersion: TLSv1_0")
-		_, err := util.Shell(`kubectl patch -n %s smcp/%s --type merge -p '{"spec":{"security":{"controlPlane":{"tls":{"minProtocolVersion":"TLSv1_0"}}}}}'`, meshNamespace, smcpName)
-		util.Shell(`oc wait --for condition=Ready -n %s smcp/%s --timeout 180s`, meshNamespace, smcpName)
-		if err != nil {
-			t.Errorf("Failed to update SMCP with tls.maxProtocolVersion: TLSv1_0")
-		}
-	})
-
-	t.Run("Operator_test_smcp_global_tls_minVersion_TLSv1_1", func(t *testing.T) {
-		defer util.RecoverPanic(t)
-
-		log.Log.Info("Update SMCP spec.security.controlPlane.tls.minProtocolVersion: TLSv1_1")
-		_, err := util.Shell(`kubectl patch -n %s smcp/%s --type merge -p '{"spec":{"security":{"controlPlane":{"tls":{"minProtocolVersion":"TLSv1_1"}}}}}'`, meshNamespace, smcpName)
-		util.Shell(`oc wait --for condition=Ready -n %s smcp/%s --timeout 180s`, meshNamespace, smcpName)
-		if err != nil {
-			t.Errorf("Failed to update SMCP with tls.maxProtocolVersion: TLSv1_1")
-		}
-	})
-
-	t.Run("Operator_test_smcp_global_tls_maxVersion_TLSv1_3", func(t *testing.T) {
-		defer util.RecoverPanic(t)
-
-		log.Log.Info("Update SMCP spec.security.controlPlane.tls.minProtocolVersion: TLSv1_3")
-		_, err := util.Shell(`kubectl patch -n %s smcp/%s --type merge -p '{"spec":{"security":{"controlPlane":{"tls":{"maxProtocolVersion":"TLSv1_3"}}}}}'`, meshNamespace, smcpName)
-		util.Shell(`oc wait --for condition=Ready -n %s smcp/%s --timeout 180s`, meshNamespace, smcpName)
-		if err != nil {
-			t.Errorf("Failed to update SMCP with tls.maxProtocolVersion: TLSv1_3")
-		}
-	})
-}
-
-func TestSSL(t *testing.T) {
-	test.NewTest(t).Id("T27").Groups(test.Full, test.InterOp).NotRefactoredYet()
-
-	defer cleanupTestSSL()
-
-	t.Run("Operator_test_smcp_testssl", func(t *testing.T) {
-		defer util.RecoverPanic(t)
-
-		// update mtls to true
-		log.Log.Info("Update SMCP mtls to true")
-		util.Shell(`kubectl patch -n %s smcp/%s --type merge -p '{"spec":{"security":{"dataPlane":{"mtls":true},"controlPlane":{"mtls":true}}}}'`, meshNamespace, smcpName)
-		util.Shell(`oc wait --for condition=Ready -n %s smcp/%s --timeout 180s`, meshNamespace, smcpName)
-
-		log.Log.Info("Update SMCP spec.security.controlPlane.tls")
-
-		util.Shell(`kubectl patch -n %s smcp/%s --type merge -p '{%s:{%s,%s,%s,%s}}}}}'`,
-			meshNamespace, smcpName,
-			`"spec":{"security":{"controlPlane":{"tls"`,
-			`"minProtocolVersion":"TLSv1_2"`,
-			`"maxProtocolVersion":"TLSv1_2"`,
-			`"cipherSuites":["TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"]`,
-			`"ecdhCurves":["CurveP256", "CurveP384"]`)
-
-		util.Shell(`oc wait --for condition=Ready -n %s smcp/%s --timeout 180s`, meshNamespace, smcpName)
-
-		log.Log.Info("Deploy bookinfo")
-		bookinfo := examples.Bookinfo{Namespace: "bookinfo"}
-		bookinfo.Install(true)
-
-		log.Log.Info("Deploy testssl pod")
-		if env.Getenv("SAMPLEARCH", "x86") == "p" {
-			util.KubeApplyContents("bookinfo", testSSLDeploymentP)
-		} else if env.Getenv("SAMPLEARCH", "x86") == "z" {
-			util.KubeApplyContents("bookinfo", testSSLDeploymentZ)
-		} else {
-			util.KubeApplyContents("bookinfo", testSSLDeployment)
-		}
-		util.CheckPodRunning("bookinfo", "app=testssl")
-
-		log.Log.Info("Check testssl.sh results. Ignore info	Command error")
-		pod, err := util.GetPodName("bookinfo", "app=testssl")
-		util.Inspect(err, "failed to get testssl pod", "", t)
-
-		command := "./testssl/testssl.sh -6 productpage:9080"
-		msg, err := util.PodExec("bookinfo", pod, "testssl", command, false)
-		util.Inspect(err, "failed to execute testssl in pod", "", t)
-		if !strings.Contains(msg, "TLSv1.2") {
-			t.Errorf("Results not include: TLSv1.2")
-			log.Log.Errorf("Results not include: TLSv1.2")
-		}
-		if !strings.Contains(msg, "ECDHE-RSA-AES128-GCM-SHA256") {
-			t.Errorf("Results not include: ECDHE-RSA-AES128-GCM-SHA256")
-			log.Log.Errorf("Results not include: ECDHE-RSA-AES128-GCM-SHA256")
-		}
-		if !strings.Contains(msg, "P-256") {
-			t.Errorf("Results not include: P-256")
-			log.Log.Errorf("Results not include: P-256")
-		}
+		t.NewSubTest("maxVersion_TLSv1_3").Run(func(t TestHelper) {
+			t.LogStep("Check to see if the SMCP maxProtocolVersion is TLSv1_3")
+			oc.Patch(t, meshNamespace, "smcp", smcpName, "merge", `{"spec":{"security":{"controlPlane":{"tls":{"maxProtocolVersion":"TLSv1_3"}}}}}`)
+			oc.WaitSMCPReady(t, meshNamespace, smcpName)
+		})
 	})
 }
---
+++ pkg/tests/ossm/testssl_test.go
@@ -0,0 +1,115 @@
+// Copyright 2021 Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package ossm
+
+import (
+	_ "embed"
+	"fmt"
+	"testing"
+
+	"github.com/maistra/maistra-test-tool/pkg/app"
+	"github.com/maistra/maistra-test-tool/pkg/util/check/assert"
+	"github.com/maistra/maistra-test-tool/pkg/util/env"
+	"github.com/maistra/maistra-test-tool/pkg/util/hack"
+	"github.com/maistra/maistra-test-tool/pkg/util/oc"
+	"github.com/maistra/maistra-test-tool/pkg/util/pod"
+	"github.com/maistra/maistra-test-tool/pkg/util/retry"
+	. "github.com/maistra/maistra-test-tool/pkg/util/test"
+)
+
+func TestSSL(t *testing.T) {
+	NewTest(t).Id("T27").Groups(Full, InterOp).Run(func(t TestHelper) {
+		hack.DisableLogrusForThisTest(t)
+		ns := "bookinfo"
+		t.Cleanup(func() {
+			oc.RecreateNamespace(t, ns)
+		})
+
+		t.LogStep("Enable the Service Mesh Control Plane mTLS to true")
+		oc.Patch(t, meshNamespace,
+			"smcp", smcpName,
+			"merge",
+			`{"spec":{"security":{"dataPlane":{"mtls":true},"controlPlane":{"mtls":true}}}}`)
+
+		t.Log("Update SMCP spec.security.controlPlane.tls")
+		oc.Patch(t, meshNamespace,
+			"smcp", smcpName,
+			"merge",
+			`{"spec":{"security":{"controlPlane":{"tls":{`+
+				`"minProtocolVersion":"TLSv1_2",`+
+				`"maxProtocolVersion":"TLSv1_2",`+
+				`"cipherSuites":["TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"],`+
+				`"ecdhCurves":["CurveP256", "CurveP384"]`+
+				`}}}}}`)
+		oc.WaitSMCPReady(t, meshNamespace, smcpName)
+
+		t.LogStep("Install bookinfo with mTLS and testssl pod")
+		oc.ApplyString(t, ns, fmt.Sprintf(testSSLDeployment, getTestSSLImage()))
+		app.InstallAndWaitReady(t, app.BookinfoWithMTLS(ns))
+		oc.WaitDeploymentRolloutComplete(t, ns, "testssl")
+
+		t.LogStep("Check testssl.sh results")
+		retry.UntilSuccess(t, func(t TestHelper) {
+			oc.Exec(t,
+				pod.MatchingSelector("app=testssl", ns),
+				"testssl",
+				"./testssl/testssl.sh -6 productpage:9080 || true",
+				assert.OutputContains(
+					"TLSv1.2",
+					"Received the TLSv1.2 needed in the testssl.sh results",
+					"Expected to receive TLSv1.2 in the testssl.sh results, but received something different"),
+				assert.OutputContains(
+					"ECDHE-RSA-AES128-GCM-SHA256",
+					"Results received the correct SHA256",
+					"Results not include: ECDHE-RSA-AES128-GCM-SHA256"),
+				assert.OutputContains(
+					"P-256",
+					"Results included: P-256",
+					"Results not include: P-256"))
+		})
+	})
+}
+
+func getTestSSLImage() string {
+	image := ""
+	switch env.Getenv("SAMPLEARCH", "x86") {
+	case "p":
+		image = "quay.io/maistra/testssl:0.0-ibm-p"
+	case "z":
+		image = "quay.io/maistra/testssl:0.0-ibm-z"
+	default:
+		image = "quay.io/maistra/testssl:latest"
+	}
+	return image
+}
+
+const testSSLDeployment = `
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: testssl
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: testssl
+  template:
+    metadata:
+      labels:
+        app: testssl
+    spec:
+      containers:
+      - name: testssl
+        image: %s
+`
