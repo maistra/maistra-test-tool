7656d2c2f095fa89261206f31314abae035564ef
Dylan Monroe <monroe.dylan.m@gmail.com>
2023-04-11 04:36:06 -0400

    OSSM-3665 Refactor TestInitContainer (T33) (#413)
    * OSSM-3665 Refactor TestInitContainer (T33)
    
    TestInitContainer:
    - Renamed to `TestInitContainerNotRemovedDuringInjection`. [CR 1]
    ---
    TestInitContainerNotRemovedDuringInjection:
    - Updated to use new test framework.
    - Updated test container success log entry to help avoid potential
    false positives.
    - Added logging for test steps. [CR 1]
    - Refactored code.
        - Moved yaml file inline.
        - Changed visibility of constants to function-level. [CR 1]
    ---
    [CR #] = updated after Code Review #
    
    Signed-off-by: Dylan Monroe <dmonroe@redhat.com>
    
    * minor fix + formatting
    
    ---------
    
    Signed-off-by: Dylan Monroe <dmonroe@redhat.com>
    Co-authored-by: Marko Lukša <marko.luksa@gmail.com>

---
+++ pkg/tests/ossm/initcontainer_test.go
@@ -0,0 +1,88 @@
+// Copyright Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ossm
+
+import (
+	"testing"
+
+	"github.com/maistra/maistra-test-tool/pkg/util/check/assert"
+	"github.com/maistra/maistra-test-tool/pkg/util/hack"
+	"github.com/maistra/maistra-test-tool/pkg/util/oc"
+	"github.com/maistra/maistra-test-tool/pkg/util/pod"
+	. "github.com/maistra/maistra-test-tool/pkg/util/test"
+)
+
+func TestInitContainerNotRemovedDuringInjection(t *testing.T) {
+	const ns = "bookinfo"
+	const goldString = "[init worked]"
+	const podSelector = "app=sleep-init"
+
+	NewTest(t).Id("T33").Groups(Full).Run(func(t TestHelper) {
+		hack.DisableLogrusForThisTest(t)
+
+		t.Log("Checking init container not removed during sidecar injection.")
+
+		t.Cleanup(func() {
+			oc.RecreateNamespace(t, ns)
+		})
+
+		oc.RecreateNamespace(t, ns)
+
+		t.LogStep("Deploying test pod.")
+		oc.ApplyString(t, ns, testInitContainerYAML)
+		oc.WaitPodReady(t, pod.MatchingSelector(podSelector, ns))
+
+		t.LogStep("Checking pod logs for init message.")
+		oc.Logs(t,
+			pod.MatchingSelector(podSelector, ns),
+			"init",
+			assert.OutputContains(goldString,
+				"Init container executed successfully.",
+				"Init container did not execute."))
+	})
+}
+
+const (
+	testInitContainerYAML = `
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: sleep-init
+spec:
+  replicas: 1
+  selector:
+    matchLabels:
+      app: sleep-init
+  template:
+    metadata:
+      annotations:
+        sidecar.istio.io/inject: "true"
+      labels:
+        app: sleep-init
+    spec:
+      terminationGracePeriodSeconds: 0
+
+      initContainers:
+      - name: init
+        image: curlimages/curl
+        command: ["/bin/echo", "[init worked]"]
+        imagePullPolicy: IfNotPresent
+
+      containers:
+      - name: sleep
+        image: curlimages/curl
+        command: ["/bin/sleep", "3650d"]
+        imagePullPolicy: IfNotPresent`
+)
