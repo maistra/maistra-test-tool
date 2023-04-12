036b6769a1a382cd51dd39fccabfa209baf01538
Praneeth Bajjuri <pbajjuri@redhat.com>
2023-04-12 01:42:31 -0500

    OSSM-3738 Refactor External Authorization (T37) (#425)
    * t37_ext-auth
    
    * fix the logstep
    
    * fixed yaml file
    
    * remove readinessprobe line
    
    ---------
    
    Co-authored-by: Praneeth Bajjuri <pbajjuri@pbajjuri.remote.csb>
    Co-authored-by: Marko Lukša <marko.luksa@gmail.com>

---
+++ pkg/tests/tasks/security/authorization/common.go
@@ -0,0 +1,58 @@
+package authorization
+
+import (
+	"fmt"
+
+	"github.com/maistra/maistra-test-tool/pkg/util/check/assert"
+	"github.com/maistra/maistra-test-tool/pkg/util/oc"
+	"github.com/maistra/maistra-test-tool/pkg/util/pod"
+	"github.com/maistra/maistra-test-tool/pkg/util/retry"
+	"github.com/maistra/maistra-test-tool/pkg/util/test"
+)
+
+func httpbinRequest(method string, path string, headers ...string) string {
+	headerArgs := ""
+	for _, header := range headers {
+		headerArgs += fmt.Sprintf(` -H "%s"`, header)
+	}
+	return fmt.Sprintf(`curl "http://httpbin:8000%s" -X %s%s -sS -o /dev/null -w "%%%%{http_code}\n"`, path, method, headerArgs)
+}
+
+func assertHttpbinRequestSucceeds(t test.TestHelper, ns string, curlCommand string) {
+	retry.UntilSuccess(t, func(t test.TestHelper) {
+		oc.Exec(t,
+			pod.MatchingSelector("app=sleep", ns),
+			"sleep",
+			curlCommand,
+			assert.OutputContains(
+				"200",
+				"Got expected 200 OK from httpbin",
+				"Expected 200 OK from httpbin, but got a different HTTP code"))
+	})
+}
+
+func assertRequestAccepted(t test.TestHelper, ns string, curlCommand string) {
+	retry.UntilSuccess(t, func(t test.TestHelper) {
+		oc.Exec(t,
+			pod.MatchingSelector("app=sleep", ns),
+			"sleep",
+			curlCommand,
+			assert.OutputContains(
+				"200",
+				"Got the expected 200 OK response for request from httpbin",
+				"Expected the AuthorizationPolicy to accept request (expected HTTP status 200), but got a different HTTP code"))
+	})
+}
+
+func assertRequestDenied(t test.TestHelper, ns string, curlCommand string) {
+	retry.UntilSuccess(t, func(t test.TestHelper) {
+		oc.Exec(t,
+			pod.MatchingSelector("app=sleep", ns),
+			"sleep",
+			curlCommand,
+			assert.OutputContains(
+				"403",
+				"Got the expected 403 Forbidden response",
+				"Expected the AuthorizationPolicy to reject request (expected HTTP status 403), but got a different HTTP code"))
+	})
+}
