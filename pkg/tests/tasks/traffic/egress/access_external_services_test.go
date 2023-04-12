f40ecbba591800ca586d313809e03d6a235d02b5
Yann Liu <yannliu@redhat.com>
2023-04-12 20:05:57 +0800

    Use json patch
    Signed-off-by: Yann Liu <yannliu@redhat.com>

--- pkg/tests/tasks/traffic/egress/access_external_services_test.go
+++ pkg/tests/tasks/traffic/egress/access_external_services_test.go
@@ -20,6 +20,7 @@ import (
 
 	"github.com/maistra/maistra-test-tool/pkg/app"
 	"github.com/maistra/maistra-test-tool/pkg/util/check/assert"
+	"github.com/maistra/maistra-test-tool/pkg/util/env"
 	"github.com/maistra/maistra-test-tool/pkg/util/hack"
 	"github.com/maistra/maistra-test-tool/pkg/util/oc"
 	"github.com/maistra/maistra-test-tool/pkg/util/test"
@@ -30,31 +31,63 @@ func TestAccessExternalServices(t *testing.T) {
 		hack.DisableLogrusForThisTest(t)
 
 		ns := "bookinfo"
+		meshNamespace := env.GetDefaultMeshNamespace()
+		smcpName := env.GetDefaultSMCPName()
+		outboundDefaultPatch := `[{"op": "remove", "path": "/spec/proxy/networking/trafficControl/outbound/policy"}]`
 		t.Cleanup(func() {
 			app.Uninstall(t, app.Sleep(ns))
+			oc.Patch(
+				t,
+				meshNamespace,
+				"smcp",
+				smcpName,
+				"json",
+				outboundDefaultPatch,
+			)
 		})
 
-		t.Log("This test validates access to external services")
+		t.Log("This test validates accesses to external services")
 
 		t.LogStepf("Install sleep into %s", ns)
 		app.InstallAndWaitReady(t, app.Sleep(ns))
 
-		t.NewSubTest("allow request to external service in default passthrough mode").Run(func(t test.TestHelper) {
-			t.LogStep("Make request to www.redhat.com from sleep")
+		t.LogStep("Make request to www.redhat.com from sleep")
+		execInSleepPod(
+			t,
+			ns,
+			buildGetRequestCmd("https://www.redhat.com/en"),
+			assert.OutputContains(
+				"200",
+				"Got expected 200 ok from www.redhat.com",
+				"Expect 200 ok from www.redhat.com, but got a different HTTP code",
+			),
+		)
 
-			execInSleepPod(
-				t,
-				ns,
-				buildGetRequestCmd("https://www.redhat.com/en"),
-				assert.OutputContains(
-					"200",
-					"Got expected 200 ok from www.redhat.com",
-					"Expect 200 ok from www.redhat.com, but got a different HTTP code",
-				),
-			)
-		})
+		outboundRegistryOnlyPatch := `[{"op": "add", "path": "/spec/proxy/networking/trafficControl/outbound/policy", "value": "REGISTRY_ONLY"}]`
 
-		t.NewSubTest("").Run(func(t test.TestHelper) {
+		t.LogStepf("Patch outbound traffic policy to registry only")
+		oc.Patch(
+			t,
+			meshNamespace,
+			"smcp",
+			smcpName,
+			"json",
+			outboundRegistryOnlyPatch,
+		)
+
+		t.LogStep("Make request to www.redhat.com from sleep again, and expect it denied")
+		execInSleepPod(
+			t,
+			ns,
+			buildGetRequestCmd("https://www.redhat.com/en"),
+			assert.OutputDoesNotContain(
+				"200",
+				"Got expected non 200 response from www.redhat.com",
+				"Expect non 200 from www.redhat.com, but got a different HTTP code",
+			),
+		)
+
+		t.NewSubTest("allow request to www.redhat.com after applying ServiceEntry").Run(func(t test.TestHelper) {
 			t.Cleanup(func() {
 				oc.DeleteFromString(t, ns, redhatExternalServiceEntryHttpsPortOnly)
 			})
@@ -75,13 +108,13 @@ func TestAccessExternalServices(t *testing.T) {
 			)
 		})
 
-		t.NewSubTest("").Run(func(t test.TestHelper) {
+		t.NewSubTest("follow access policies for httpbin.org").Run(func(t test.TestHelper) {
 			t.Cleanup(func() {
 				oc.DeleteFromString(t, ns, httpbinExternalServiceEntryHttpPortOnly)
 				oc.DeleteFromString(t, ns, httpbinExternalVituralServiceWithTimeout)
 			})
 
-			t.LogStep("Apply a ServiceEntry to httpbin.org")
+			t.LogStep("Apply a ServiceEntry to httpbin.org with only HTTP port")
 			oc.ApplyString(t, ns, httpbinExternalServiceEntryHttpPortOnly)
 
 			t.LogStep("Send a request to httpbin.org on HTTP port")
@@ -93,6 +126,18 @@ func TestAccessExternalServices(t *testing.T) {
 					"200",
 					"Got expetcted 200 ok from httpbin.org",
 					"Expect 200 ok from httpbin.org, but got a different HTTP code",
+				),
+			)
+
+			t.LogStep("Send a request to httpbin.org on HTTPS port")
+			execInSleepPod(
+				t,
+				ns,
+				buildGetRequestCmd("https://httpbin.org/headers"),
+				assert.OutputDoesNotContain(
+					"200",
+					"Got expetcted non 200 response from httpbin.org",
+					"Expect non 200 from httpbin.org, but got a different HTTP code",
 				),
 			)
 
