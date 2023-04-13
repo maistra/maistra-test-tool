43790456e822563ee06345e4a89d7abbf213a8fd
Francisco Herrera <frherrer@redhat.com>
2023-04-13 13:34:41 +0200

    move retry to retry request again

--- pkg/tests/tasks/traffic/circuit_breaking_test.go
+++ pkg/tests/tasks/traffic/circuit_breaking_test.go
@@ -58,28 +58,28 @@ func TestCircuitBreaking(t *testing.T) {
 		})
 
 		t.LogStep("Tripping the circuit breaker")
-		t.Log("To tipping the circuit breaker we are going to send 50 requests to httpbin with 2 connections")
 		connection := 2
 		reqCount := 50
-		msg := oc.Exec(t,
-			pod.MatchingSelector("app=fortio", ns),
-			"fortio",
-			fmt.Sprintf("/usr/bin/fortio load -c %d -qps 0 -n %d -loglevel Warning http://httpbin:8000/get", connection, reqCount))
-
-		t.LogStep("Validate the number of 200 responses")
-		t.Log("verify from output message the number of 200 responses to the load test. We expect to have a line with this information: Code 200 : XX (X0.0 %)")
-		c200 := getNumberOfResponses(t, msg, `Code 200.*`)
-
-		t.LogStep("Validate the number of 503 responses")
-		t.Log("verify from output message the number of 500 responses to the load test. We expect to have a line with this information: Code 503 : XX (X0.0 %)")
-		c503 := getNumberOfResponses(t, msg, `Code 503.*`)
-
-		t.LogStep("Validate the percentage of 200 responses and 503 to the total of requests")
-		t.Log("We expect to have 60% 200 responses and 40% 503 responses")
 		tolerance := 0.1
-		successRate200 := float64(c200) / float64(reqCount) * 100
-		successRate503 := float64(c503) / float64(reqCount) * 100
 		retry.UntilSuccess(t, func(t test.TestHelper) {
+			t.Log("To tipping the circuit breaker we are going to send 50 requests to httpbin with 2 connections")
+			msg := oc.Exec(t,
+				pod.MatchingSelector("app=fortio", ns),
+				"fortio",
+				fmt.Sprintf("/usr/bin/fortio load -c %d -qps 0 -n %d -loglevel Warning http://httpbin:8000/get", connection, reqCount))
+
+			t.LogStep("Validate the number of 200 responses")
+			t.Log("We expect to have a line with this information: Code 200 : XX (X0.0 %)")
+			c200 := getNumberOfResponses(t, msg, `Code 200.*`)
+
+			t.LogStep("Validate the number of 503 responses")
+			t.Log("We expect to have a line with this information: Code 503 : XX (X0.0 %)")
+			c503 := getNumberOfResponses(t, msg, `Code 503.*`)
+
+			t.LogStep("Validate the percentage of 200 responses and 503 to the total of requests")
+			t.Log("We expect to have 60% 200 responses and 40% 503 responses")
+			successRate200 := float64(c200) / float64(reqCount) * 100
+			successRate503 := float64(c503) / float64(reqCount) * 100
 			if util.IsWithinPercentage(c200, reqCount, 0.6, tolerance) && util.IsWithinPercentage(c503, reqCount, 0.4, tolerance) {
 				t.Logf(
 					"Success. Circuit breaking acts as expected. "+
