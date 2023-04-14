0d7980d18b1279554a62b4fec42dd91984cb6ca9
Mayleigh M <98288958+mayleighnmyers@users.noreply.github.com>
2023-04-14 15:30:25 -0400

    OSSM-3742 Refactor TestEgressWildcard (T16) (#433)
    * OSSM-3742 Reactor TestEgressWildcard (T16)
    
    * fix major syntax errors
    
    * Removed GetPodName
    
    * Made changes based on comments
    
    * Made changes
    
    * add cleanup
    
    * minor improvement to log messages
    
    ---------
    
    Co-authored-by: Marko Lukša <marko.luksa@gmail.com>

--- pkg/tests/tasks/traffic/egress/egress_wildcard_hosts_test.go
+++ pkg/tests/tasks/traffic/egress/egress_wildcard_hosts_test.go
@@ -15,76 +15,164 @@
 package egress
 
 import (
-	"strings"
 	"testing"
-	"time"
 
-	"github.com/maistra/maistra-test-tool/pkg/examples"
-	"github.com/maistra/maistra-test-tool/pkg/util"
-	"github.com/maistra/maistra-test-tool/pkg/util/log"
-	"github.com/maistra/maistra-test-tool/pkg/util/test"
+	"github.com/maistra/maistra-test-tool/pkg/app"
+	"github.com/maistra/maistra-test-tool/pkg/util/check/assert"
+	"github.com/maistra/maistra-test-tool/pkg/util/hack"
+	"github.com/maistra/maistra-test-tool/pkg/util/oc"
+	"github.com/maistra/maistra-test-tool/pkg/util/pod"
+	"github.com/maistra/maistra-test-tool/pkg/util/retry"
+	. "github.com/maistra/maistra-test-tool/pkg/util/test"
 )
 
-func cleanupEgressWildcard() {
-	log.Log.Info("Cleanup")
-	util.KubeDeleteContents("bookinfo", util.RunTemplate(EgressWildcardGatewayTemplate, smcp))
-	util.KubeDeleteContents("bookinfo", EgressWildcardEntry)
-	sleep := examples.Sleep{Namespace: "bookinfo"}
-	sleep.Uninstall()
-	time.Sleep(time.Duration(20) * time.Second)
-}
-
 func TestEgressWildcard(t *testing.T) {
-	test.NewTest(t).Id("T16").Groups(test.Full, test.InterOp).NotRefactoredYet()
+	NewTest(t).Id("T16").Groups(Full, InterOp).Run(func(t TestHelper) {
+		t.Log("This test checks if the wildcard in the ServiceEntry and Gateway works as expected for Egress traffic.")
+		hack.DisableLogrusForThisTest(t)
 
-	defer cleanupEgressWildcard()
-	defer util.RecoverPanic(t)
+		ns := "bookinfo"
 
-	log.Log.Info("Test Egress Wildcard Hosts")
-	sleep := examples.Sleep{Namespace: "bookinfo"}
-	sleep.Install()
-	sleepPod, err := util.GetPodName("bookinfo", "app=sleep")
-	util.Inspect(err, "Failed to get sleep pod name", "", t)
+		t.LogStep("Install the sleep pod")
+		app.InstallAndWaitReady(t, app.Sleep(ns))
+		t.Cleanup(func() {
+			app.Uninstall(t, app.Sleep(ns))
+		})
 
-	t.Run("TrafficManagement_egress_direct_traffic_wildcard_host", func(t *testing.T) {
-		defer util.RecoverPanic(t)
+		t.NewSubTest("ServiceEntry").Run(func(t TestHelper) {
+			t.LogStep("Configure ServiceEntry with wildcard host *.wikipedia.org")
+			oc.ApplyString(t, ns, EgressWildcardServiceEntry)
+			t.Cleanup(func() {
+				oc.DeleteFromString(t, ns, EgressWildcardServiceEntry)
+			})
 
-		log.Log.Info("Configure direct traffic to a wildcard host")
-		util.KubeApplyContents("bookinfo", EgressWildcardEntry)
-		time.Sleep(time.Duration(10) * time.Second)
+			assertExternalRequestSuccess(t, ns)
+		})
 
-		command := `curl -s https://en.wikipedia.org/wiki/Main_Page | grep -o "<title>.*</title>"; curl -s https://de.wikipedia.org/wiki/Wikipedia:Hauptseite | grep -o "<title>.*</title>"`
-		msg, err := util.PodExec("bookinfo", sleepPod, "sleep", command, false)
-		util.Inspect(err, "Failed to get response", "", t)
-		if strings.Contains(msg, "<title>Wikipedia, the free encyclopedia</title>\n<title>Wikipedia – Die freie Enzyklopädie</title>") {
-			log.Log.Infof("Success. Got Wikipedia response: %s", msg)
-		} else {
-			log.Log.Infof("Error response: %s", msg)
-			t.Errorf("Error response: %s", msg)
-		}
+		t.NewSubTest("Gateway").Run(func(t TestHelper) {
+			t.LogStep("Configure egress Gateway with wildcard host *.wikipedia.org")
+			oc.ApplyTemplate(t, ns, EgressWildcardGatewayTemplate, smcp)
+			t.Cleanup(func() {
+				oc.DeleteFromTemplate(t, ns, EgressWildcardGatewayTemplate, smcp)
+			})
 
-		util.KubeDeleteContents("bookinfo", EgressWildcardEntry)
+			assertExternalRequestSuccess(t, ns)
+		})
 	})
-
-	t.Run("TrafficManagement_egress_gateway_wildcard_host", func(t *testing.T) {
-		defer util.RecoverPanic(t)
-
-		log.Log.Info("Configure egress gateway to a wildcard host")
-		util.KubeApplyContents("bookinfo", util.RunTemplate(EgressWildcardGatewayTemplate, smcp))
-		time.Sleep(time.Duration(10) * time.Second)
-
-		command := `curl -s https://en.wikipedia.org/wiki/Main_Page | grep -o "<title>.*</title>"; curl -s https://de.wikipedia.org/wiki/Wikipedia:Hauptseite | grep -o "<title>.*</title>"`
-		msg, err := util.PodExec("bookinfo", sleepPod, "sleep", command, false)
-		util.Inspect(err, "Failed to get response", "", t)
-		if strings.Contains(msg, "<title>Wikipedia, the free encyclopedia</title>\n<title>Wikipedia – Die freie Enzyklopädie</title>") {
-			log.Log.Infof("Success. Got Wikipedia response: %s", msg)
-		} else {
-			log.Log.Infof("Error response: %s", msg)
-			t.Errorf("Error response: %s", msg)
-		}
-
-		util.KubeDeleteContents("bookinfo", util.RunTemplate(EgressWildcardGatewayTemplate, smcp))
-	})
-
-	// setup SNI proxy for wildcard arbitrary domains
 }
+
+func assertExternalRequestSuccess(t TestHelper, ns string) {
+	t.LogStep("Check external request to en.wikipedia.org and de.wikipedia.org")
+	retry.UntilSuccess(t, func(t TestHelper) {
+		oc.Exec(t,
+			pod.MatchingSelector("app=sleep", ns),
+			"sleep",
+			`curl -s https://en.wikipedia.org/wiki/Main_Page`,
+			assert.OutputContains(
+				"<title>Wikipedia, the free encyclopedia</title>",
+				"Received the correct response from en.wikipedia.org",
+				"Failed to receive the correct response from en.wikipedia.org"))
+
+		oc.Exec(t,
+			pod.MatchingSelector("app=sleep", ns),
+			"sleep",
+			`curl -s https://de.wikipedia.org/wiki/Wikipedia:Hauptseite`,
+			assert.OutputContains(
+				"<title>Wikipedia – Die freie Enzyklopädie</title>",
+				"Received the correct response from de.wikipedia.org",
+				"Failed to receive the correct response from de.wikipedia.org"))
+	})
+}
+
+const (
+	EgressWildcardServiceEntry = `
+apiVersion: networking.istio.io/v1alpha3
+kind: ServiceEntry
+metadata:
+  name: wikipedia
+spec:
+  hosts:
+  - "*.wikipedia.org"
+  ports:
+  - number: 443
+    name: https
+    protocol: HTTPS
+`
+	EgressWildcardGatewayTemplate = `
+apiVersion: networking.istio.io/v1alpha3
+kind: Gateway
+metadata:
+  name: istio-egressgateway
+spec:
+  selector:
+    istio: egressgateway
+  servers:
+  - port:
+      number: 443
+      name: https
+      protocol: HTTPS
+    hosts:
+    - "*.wikipedia.org"
+    tls:
+      mode: PASSTHROUGH
+---
+apiVersion: networking.istio.io/v1alpha3
+kind: DestinationRule
+metadata:
+  name: egressgateway-for-wikipedia
+spec:
+  host: istio-egressgateway.{{ .Namespace }}.svc.cluster.local
+  subsets:
+    - name: wikipedia
+---
+apiVersion: networking.istio.io/v1alpha3
+kind: VirtualService
+metadata:
+  name: direct-wikipedia-through-egress-gateway
+spec:
+  hosts:
+  - "*.wikipedia.org"
+  gateways:
+  - mesh
+  - istio-egressgateway
+  tls:
+  - match:
+    - gateways:
+      - mesh
+      port: 443
+      sniHosts:
+      - "*.wikipedia.org"
+    route:
+    - destination:
+        host: istio-egressgateway.{{ .Namespace }}.svc.cluster.local
+        subset: wikipedia
+        port:
+          number: 443
+      weight: 100
+  - match:
+    - gateways:
+      - istio-egressgateway
+      port: 443
+      sniHosts:
+      - "*.wikipedia.org"
+    route:
+    - destination:
+        host: www.wikipedia.org
+        port:
+          number: 443
+      weight: 100
+---
+apiVersion: networking.istio.io/v1alpha3
+kind: ServiceEntry
+metadata:
+  name: www-wikipedia
+spec:
+  hosts:
+  - www.wikipedia.org
+  ports:
+  - number: 443
+    name: https
+    protocol: HTTPS
+  resolution: DNS
+`
+)
