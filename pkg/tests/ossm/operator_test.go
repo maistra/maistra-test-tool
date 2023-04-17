4064f42e32d632b8a6b012f1517c62fc9f980b74
Francisco Herrera <frherrer@redhat.com>
2023-04-17 18:11:33 +0200

    T40 new test case

---
+++ pkg/tests/ossm/operator_test.go
@@ -0,0 +1,134 @@
+// Copyright Red Hat, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ossm
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/maistra/maistra-test-tool/pkg/util/check/assert"
+	"github.com/maistra/maistra-test-tool/pkg/util/oc"
+	"github.com/maistra/maistra-test-tool/pkg/util/pod"
+	"github.com/maistra/maistra-test-tool/pkg/util/retry"
+	"github.com/maistra/maistra-test-tool/pkg/util/shell"
+	"github.com/maistra/maistra-test-tool/pkg/util/test"
+	. "github.com/maistra/maistra-test-tool/pkg/util/test"
+)
+
+var workername string
+
+// TestOperator tests scenario to cover all the test cases related to the OSSM operators
+func TestOperator(t *testing.T) {
+	NewTest(t).Id("T40").Groups(Full).Run(func(t TestHelper) {
+		t.Log("Test related to OSSM Operators and Istio resources")
+		if Smcp.Rosa {
+			t.Skip("Skipping test on ROSA") // Now in Rosa this test need to be skipped due to lack of permissions in ROSA cluster
+		}
+		t.Cleanup(func() {
+			oc.Label(t, "", "node", workername, "node-role.kubernetes.io/infra-")
+			oc.Label(t, "", "node", workername, "node-role.kubernetes.io-")
+		})
+
+		t.LogStep("Setup: Get a worker node from the cluster that does not have the istio operator installed, label it as infra")
+		workername = pickWorkerNode(t)
+		oc.Label(t, "", "node", workername, "node-role.kubernetes.io/infra=")
+		oc.Label(t, "", "node", workername, "ode-role.kubernetes.io=infra")
+
+		// Reference: https://issues.redhat.com/browse/OSSM-2342
+		t.NewSubTest("Run on Infra Nodes").Run(func(t TestHelper) {
+			t.Log("Testing: Run OSSM Operator on infra nodes")
+			t.Cleanup(func() {
+				// Untaint the infra node and remove modification in subscription
+				shell.Execute(t,
+					`oc adm taint nodes -l node-role.kubernetes.io/infra node-role.kubernetes.io/infra=reserved:NoSchedule- node-role.kubernetes.io/infra=reserved:NoExecute-`)
+				shell.Execute(t,
+					`oc patch subscription servicemeshoperator -n openshift-operators --type json -p='[{"op": "remove", "path": "/spec/config/tolerations"}]'`)
+				oc.WaitSMCPReady(t, meshNamespace, smcpName) // Wait for the SMCP to be ready before move to next subtest case (Because untaint)
+			})
+			t.LogStep("Taint node and edit the subscription to add the infra node to the node selector")
+			shell.Execute(t,
+				`oc adm taint nodes -l node-role.kubernetes.io/infra node-role.kubernetes.io/infra=reserved:NoSchedule node-role.kubernetes.io/infra=reserved:NoExecute`)
+			oc.Patch(t,
+				"openshift-operators",
+				"subscription",
+				"servicemeshoperator",
+				"merge",
+				`{"spec":{"config":{"nodeSelector":{"node-role.kubernetes.io/infra":""},"tolerations":[{"effect":"NoSchedule","key":"node-role.kubernetes.io/infra","value":"reserved"},{"effect":"NoExecute","key":"node-role.kubernetes.io/infra","value":"reserved"}]}}}`)
+
+			t.LogStep(fmt.Sprintf("Verify operator pod is running on the infra node. Node expected: %s", workername))
+			cmd := fmt.Sprintf(
+				`oc get pods -n openshift-operators -l name=istio-operator --field-selector spec.nodeName=%s -o jsonpath='{.items[0].metadata.name}'`,
+				workername)
+			retry.UntilSuccess(t, func(t test.TestHelper) {
+				operatorPod := pod.MatchingSelector("name=istio-operator", "openshift-operators")
+				oc.WaitPodReady(t, operatorPod)
+				shell.Execute(t,
+					cmd,
+					assert.OutputContains(
+						"istio-operator-",
+						"Sucess: Operator pod is running on the infra node",
+						"Error: Operator pod is not running on the infra node"))
+			})
+		})
+
+		// Reference: https://issues.redhat.com/browse/OSSM-3516
+		t.NewSubTest("Run SMCP Infra Nodes").Run(func(t TestHelper) {
+			t.Log("Testing: Run OSSM Operator on infra nodes")
+			t.Cleanup(func() {
+				shell.Executef(t,
+					`kubectl -n %s patch smcp/%s --type=json -p='[{"op": "remove", "path": "/spec/runtime"}]'`,
+					meshNamespace,
+					smcpName)
+				oc.WaitSMCPReady(t, meshNamespace, smcpName) // Avoid go to the next test with the SMCP not ready
+			})
+
+			t.LogStep("Patch SMCP to run on infra nodes and wait for the SMCP to be ready")
+			oc.Patch(t, meshNamespace, "smcp", smcpName, "merge",
+				fmt.Sprintf(`oc -n %s patch smcp/%s --type merge -p '{"spec":{"runtime":{"defaults":{"pod":{"nodeSelector":{"node-role.kubernetes.io/infra":""},"tolerations":[{"effect":"NoSchedule","key":"node-role.kubernetes.io/infra","value":"reserved"},{"effect":"NoExecute","key":"node-role.kubernetes.io/infra","value":"reserved"}]}}}}}'`,
+					meshNamespace,
+					smcpName))
+			oc.WaitSMCPReady(t, meshNamespace, smcpName)
+
+			t.LogStep("Verify that the smcp pods are running on the infra node")
+			retry.UntilSuccess(t, func(t test.TestHelper) {
+				nsPods := shell.Executef(t, `oc get pods -n %s -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}'`, meshNamespace)
+				for pod := range nsPods {
+					// Verify each pod is running on the infra node
+					node := shell.Executef(t, `oc get pod -n %s %s -o jsonpath='{.spec.nodeName}'`, meshNamespace, pod)
+					if node != workername {
+						// For some reason not all pods are relocated automatically, so we need to delete the pod and wait for it to be recreated
+						shell.Executef(t, `oc delete pod -n %s %s`, meshNamespace, pod)
+						shell.Executef(t, `kubectl -n %s wait --for condition=Ready pod %s --timeout 30s || true`, meshNamespace, pod)
+						node = shell.Executef(t, `oc get pod -n %s %s -o jsonpath='{.spec.nodeName}'`, meshNamespace, pod)
+						if node != workername {
+							t.Fatalf("Pod %s is not running on the infra node", pod)
+						}
+					}
+				}
+			})
+		})
+	})
+}
+
+func pickWorkerNode(t test.TestHelper) string {
+	workername := shell.Execute(t, "oc get nodes -l node-role.kubernetes.io/worker= -o jsonpath='{.items[0].metadata.name}'")
+	actualNode := shell.Execute(t, `oc get pods -n openshift-operators -l name=istio-operator -o jsonpath='{.items[0].spec.nodeName}'`)
+	if workername == actualNode {
+		// If the worker node is the same as the node where the operator is running, pick the second worker node
+		workername = shell.Execute(t, "oc get nodes -l node-role.kubernetes.io/worker= -o jsonpath='{.items[1].metadata.name}'")
+	}
+
+	return workername
+}
