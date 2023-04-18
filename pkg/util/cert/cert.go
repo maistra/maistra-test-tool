f12331c8f374a71d8501af68d4e733885aa166d6
Yuanlin <yuanlin.xu@redhat.com>
2023-04-18 19:33:40 -0400

    Add references

--- pkg/util/cert/cert.go
+++ pkg/util/cert/cert.go
@@ -1,17 +1,3 @@
-// Copyright 2023 Red Hat, Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package cert
 
 import (
@@ -172,3 +158,8 @@ func (cf *CertBuilder) SetParent(parent *x509.Certificate) {
 func (cf *CertBuilder) SetParentKey(privateKey *rsa.PrivateKey) {
 	cf.parentKey = privateKey
 }
+
+/* References:
+https://gist.github.com/shaneutt/5e1995295cff6721c89a71d13a71c251
+https://gist.github.com/Mattemagikern/328cdd650be33bc33105e26db88e487d
+*/
