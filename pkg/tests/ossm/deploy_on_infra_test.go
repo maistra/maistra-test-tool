c7d2e6c9388a1414c25022879474c5fb3590054c
Francisco Herrera <frherrer@redhat.com>
2023-04-21 15:15:53 +0200

    skip T40 test case

--- pkg/tests/ossm/deploy_on_infra_test.go
+++ pkg/tests/ossm/deploy_on_infra_test.go
@@ -35,6 +35,7 @@ var workername string
 func TestDeployOnInfraNodes(t *testing.T) {
 	NewTest(t).Id("T40").Groups(Full).Run(func(t TestHelper) {
 		t.Log("This test verifies that the OSSM operator and Istio components can be configured to run on infrastructure nodes")
+		t.Skip("Skipping test due to https://issues.redhat.com/browse/OSSM-3837")
 		if env.GetSMCPVersion().LessThan(version.SMCP_2_3) {
 			t.Skip("Deploy On Infra node is available in SMCP versions v2.3+")
 		}
