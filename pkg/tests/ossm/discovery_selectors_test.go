af1704be2aeb7b9009106cf12ae9d7c7abe80c11
mamyers <mamyers@redhat.com>
2023-04-24 16:48:27 -0400

    OSSM-3802 Finished Test

--- pkg/tests/ossm/discovery_selectors_test.go
+++ pkg/tests/ossm/discovery_selectors_test.go
@@ -19,8 +19,8 @@ import (
 
 	"github.com/maistra/maistra-test-tool/pkg/app"
 	"github.com/maistra/maistra-test-tool/pkg/util/check/assert"
+	"github.com/maistra/maistra-test-tool/pkg/util/ns"
 	"github.com/maistra/maistra-test-tool/pkg/util/oc"
-	"github.com/maistra/maistra-test-tool/pkg/util/pod"
 	"github.com/maistra/maistra-test-tool/pkg/util/retry"
 	"github.com/maistra/maistra-test-tool/pkg/util/shell"
 	. "github.com/maistra/maistra-test-tool/pkg/util/test"
@@ -30,17 +30,25 @@ func TestDiscoverySelectors(t *testing.T) {
 	NewTest(t).Groups(Full).Run(func(t TestHelper) {
 		t.Log("This test checks if discoverySelectors are being honored")
 
-		ns := "bookinfo"
-
 		DeployControlPlane(t)
 
 		t.LogStep("Install httpbin and sleep pod")
-		app.InstallAndWaitReady(t, app.Httpbin(ns), app.Sleep(ns))
+		app.InstallAndWaitReady(t, app.Sleep(ns.Foo), app.Httpbin(ns.MeshExternal))
 		t.Cleanup(func() {
-			app.Uninstall(t, app.Httpbin(ns), app.Sleep(ns))
+			app.Uninstall(t, app.Sleep(ns.Foo), app.Httpbin(ns.MeshExternal))
 		})
 
-		oc.Label(t, "", "namespace", ns, "istio-discovery=enabled")
+		retry.UntilSuccess(t, func(t TestHelper) {
+			shell.Execute(t, "istioctl pc endpoint deploy/sleep",
+				assert.OutputContains("sleep",
+					"Sleep was discovered",
+					"Expected sleep to be discovered, but it was not found"),
+				assert.OutputContains("httpbin",
+					"Httpbin was discovered",
+					"Expected Httpbin to be discovered, but it was not found."))
+		})
+
+		oc.Label(t, "", "namespace", ns.Foo, "istio-discovery=enabled")
 
 		oc.Patch(t, meshNamespace, "smcp", smcpName, "merge", `
 spec:
@@ -58,59 +66,14 @@ spec:
 
 		oc.WaitSMCPReady(t, meshNamespace, smcpName)
 
-		shell.Execute(t, "istioctl pc endpoint deploy/sleep",
-			assert.OutputDoesNotContain("httpbin",
-				"Httpbin was not discovered",
-				"Expected Httpbin to not be discovered, but it was."))
-
-		t.NewSubTest("discoverySelectors").Run(func(t TestHelper) {
-			t.LogStep("Update SMCP spec.controlPlane.meshConfig.discoverySelectors: Enabled")
-			oc.Patch(t, meshNamespace, "smcp", smcpName, "merge", `
-spec:
-  meshConfig:
-    discoverySelectors:
-    - matchLabels:
-        istio-discovery: enabled`)
-			oc.WaitSMCPReady(t, meshNamespace, smcpName)
-		})
-
-		t.NewSubTest("Convertion").Run(func(t TestHelper) {
-			t.LogStep("Convert the Helm Values to smcp spec Values")
-			t.Cleanup(func() {
-				oc.DeleteFromString(t, ns, DSEnabled)
-			})
-			oc.ApplyString(t, ns, DSEnabled)
-			t.LogStep("Verify a request to path /ip is allowed")
-			assertHttpbinRequestSucceeds(t, ns, httpbinRequest("GET", "/ip"))
-
+		retry.UntilSuccess(t, func(t TestHelper) {
+			shell.Execute(t, "istioctl pc endpoint deploy/sleep",
+				assert.OutputContains("sleep",
+					"Sleep was discovered",
+					"Expected sleep to be discovered, but it was not found"),
+				assert.OutputDoesNotContain("httpbin",
+					"Httpbin was not discovered",
+					"Expected Httpbin to not be discovered, but it was."))
 		})
 	})
 }
-
-func assertHttpbinRequestSucceeds(t TestHelper, ns string) {
-	t.LogStep("Check if the httpbin returns 200 OK")
-	retry.UntilSuccess(t, func(t TestHelper) {
-		oc.Exec(t,
-			pod.MatchingSelector("app=httpbin", ns),
-			"httpbin",
-			`curl http://httpbin:8000/ip -s -o /dev/null -w "%{http_code}"`,
-			assert.OutputContains(
-				"200",
-				"Got expected 200 OK from httpbin",
-				"Expected 200 OK from httpbin, but got a different HTTP code"))
-	})
-}
-
-const (
-	DSEnabled = `
-apiVersion: install.istio.io/v1alpha1
-kind: ServiceMeshControlPlane
-metadata:
-  namespace: istio-system
-spec:
-	# You may override parts of meshconfig by uncommenting the following lines.
-  meshConfig:
-    discoverySelectors:
-    - matchLabels:
-        istio-discovery: enabled`
-)
