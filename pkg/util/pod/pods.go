b25d42949d24df13f17156d5442b2514c7f9c3c3
Francisco Herrera Lira <fjglira@gmail.com>
2023-05-10 14:37:31 +0200

    Fix TestOpenShiftMonitoring (#549)
    * fix TestOpenShiftMonitoring
    
    * improve code and adding MatchingSelectorFirst
    
    ---------
    
    Co-authored-by: Francisco Herrera <frherrer@redhat.com>

--- pkg/util/pod/pods.go
+++ pkg/util/pod/pods.go
@@ -7,20 +7,38 @@ import (
 	"github.com/maistra/maistra-test-tool/pkg/util/test"
 )
 
+func getPods(t test.TestHelper, oc *ocpackage.OC, selector string, ns string) []ocpackage.NamespacedName {
+	t.T().Helper()
+	output := oc.Invokef(t, "kubectl -n %s get pods -l %q -o jsonpath='{.items[*].metadata.name}'", ns, selector)
+	if output == "" {
+		t.Fatalf("no pods found using selector %s in namespace %s", selector, ns)
+	}
+	pods := strings.Split(output, " ")
+	var namespacedNames []ocpackage.NamespacedName
+	for _, pod := range pods {
+		namespacedNames = append(namespacedNames, ocpackage.NewNamespacedName(ns, pod))
+	}
+	return namespacedNames
+}
+
 func MatchingSelector(selector string, ns string) ocpackage.PodLocatorFunc {
 	return func(t test.TestHelper, oc *ocpackage.OC) ocpackage.NamespacedName {
 		t.T().Helper()
-		output := oc.Invokef(t, "kubectl -n %s get pods -l %q -o jsonpath='{.items[*].metadata.name}'", ns, selector)
-		if output == "" {
-			t.Fatalf("no pods found using selector %s in namespace %s", selector, ns)
-		}
-		pods := strings.Split(output, " ")
+		pods := getPods(t, oc, selector, ns)
 		if len(pods) == 1 {
-			return ocpackage.NewNamespacedName(ns, pods[0])
+			return pods[0]
 		} else {
 			t.Fatalf("more than one pod found using selector %q in namespace %s: %v", selector, ns, pods)
 			panic("should never reach this point because the preceding code either returns or calls t.Fatalf(), which causes a panic")
 		}
 
+	}
+}
+
+func MatchingSelectorFirst(selector string, ns string) ocpackage.PodLocatorFunc {
+	return func(t test.TestHelper, oc *ocpackage.OC) ocpackage.NamespacedName {
+		t.T().Helper()
+		pods := getPods(t, oc, selector, ns)
+		return pods[0]
 	}
 }
