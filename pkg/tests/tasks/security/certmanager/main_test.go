81b1967cb640afe099da0e8a60f0ec2958f6f7e5
Jacek Ewertowski <jewertow@redhat.com>
2023-05-18 11:13:07 +0200

    OSSM-3962: Add test for cacerts secret provided by cert-manager (#555)
    * OSSM-3962: Add e2d test for cacerts provided by cert-manager
    
    Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>
    
    * OSSM-3962: Move cert-manager test to a dedicated package
    
    Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>
    
    * Add support for Cleanup function to setupTestHelper
    
    Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>
    
    * Implement test for cacerts provided by cert-manager
    
    Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>
    
    * Create certificates and issuers without additional retries
    
    Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>
    
    ---------
    
    Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>

---
+++ pkg/tests/tasks/security/certmanager/main_test.go
@@ -0,0 +1,67 @@
+package certmanager
+
+import (
+	_ "embed"
+	"testing"
+
+	"github.com/maistra/maistra-test-tool/pkg/tests/ossm"
+	"github.com/maistra/maistra-test-tool/pkg/util/env"
+	"github.com/maistra/maistra-test-tool/pkg/util/oc"
+	"github.com/maistra/maistra-test-tool/pkg/util/pod"
+	"github.com/maistra/maistra-test-tool/pkg/util/test"
+)
+
+var (
+	smcpName              = env.GetDefaultSMCPName()
+	meshNamespace         = env.GetDefaultMeshNamespace()
+	certManagerOperatorNs = "cert-manager-operator"
+	certManagerNs         = "cert-manager"
+
+	//go:embed yaml/cert-manager-operator.yaml
+	certManagerOperator string
+
+	//go:embed yaml/root-ca.yaml
+	rootCA string
+
+	//go:embed yaml/istio-csr/istio-csr.yaml
+	istioCsrTmpl string
+
+	//go:embed yaml/istio-csr/mesh.yaml
+	serviceMeshIstioCsrTmpl string
+
+	//go:embed yaml/istio-csr/istio-ca.yaml
+	istioCA string
+
+	//go:embed yaml/cacerts/mesh.yaml
+	serviceMeshCacertsTmpl string
+
+	//go:embed yaml/cacerts/cacerts.yaml
+	cacerts string
+)
+
+func TestMain(m *testing.M) {
+	test.NewSuite(m).
+		Setup(setupCertManagerOperator).
+		Run()
+}
+
+func setupCertManagerOperator(t test.TestHelper) {
+	t.Cleanup(func() {
+		oc.DeleteFromString(t, certManagerNs, rootCA)
+		oc.DeleteFromString(t, certManagerOperatorNs, certManagerOperator)
+		oc.DeleteNamespace(t, certManagerOperatorNs)
+		oc.DeleteNamespace(t, certManagerNs)
+	})
+	ossm.BasicSetup(t)
+
+	t.LogStep("Create namespace for cert-manager-operator")
+	oc.CreateNamespace(t, certManagerOperatorNs)
+
+	t.LogStep("Install cert-manager-operator")
+	oc.ApplyString(t, certManagerOperatorNs, certManagerOperator)
+	oc.WaitPodReady(t, pod.MatchingSelector("name=cert-manager-operator", certManagerOperatorNs))
+	oc.WaitPodReady(t, pod.MatchingSelector("app=cert-manager", certManagerNs))
+
+	t.LogStep("Create root ca")
+	oc.ApplyString(t, certManagerNs, rootCA)
+}
