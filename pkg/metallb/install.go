049c6a00ac37363000e359492681cc83094078da
Jacek Ewertowski <jewertow@redhat.com>
2023-05-30 00:03:54 +0200

    Refactor MetalLB installer
    Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>

--- pkg/metallb/install.go
+++ pkg/metallb/install.go
@@ -2,9 +2,11 @@ package metallb
 
 import (
 	_ "embed"
+	"fmt"
 	"net"
 	"strings"
 
+	"github.com/maistra/maistra-test-tool/pkg/util/check/assert"
 	"github.com/maistra/maistra-test-tool/pkg/util/ns"
 	"github.com/maistra/maistra-test-tool/pkg/util/oc"
 	"github.com/maistra/maistra-test-tool/pkg/util/retry"
@@ -18,63 +20,84 @@ var (
 
 	//go:embed yaml/metallb.yaml
 	metallb string
-
-	//go:embed yaml/ip-addess-pool.tmpl.yaml
-	ipAddressPoolTmpl string
 )
 
-func InstallIfNotExist(t test.TestHelper) {
-	t.Log("Check if MetalLB already exists")
+func InstallIfNotExist(t test.TestHelper, kubeConfigs ...string) {
+	if len(kubeConfigs) == 0 {
+		installWithOC(t, *oc.DefaultOC)
+		return
+	}
+
+	for _, config := range kubeConfigs {
+		oc := oc.WithKubeconfig(config)
+		if oc == nil {
+			t.Errorf("failed to create oc config from kubeconfig file: %s", config)
+		}
+		installWithOC(t, *oc)
+	}
+}
+
+func installWithOC(t test.TestHelper, oc oc.OC) {
+	t.Log("Check if MetalLB operator already exists")
 	output := oc.Get(t, ns.MetalLB, "deployments", "metallb-operator-controller-manager")
 	if !strings.Contains(output, "Error from server (NotFound)") {
 		t.Log("MetalLB operator already exists - skipping installation")
-		return
+	} else {
+		installOperator(t, oc)
 	}
-	installOperator(t)
-	deployMetalLB(t)
-	createAddressPool(t)
+	deployMetalLB(t, oc)
+	createAddressPool(t, oc)
 }
 
-func installOperator(t test.TestHelper) {
+func installOperator(t test.TestHelper, oc oc.OC) {
 	t.Log("Install MetalLB operator")
 	oc.ApplyString(t, ns.MetalLB, metallbOperator)
 	retry.UntilSuccess(t, func(t test.TestHelper) {
-		output := oc.Get(t, ns.MetalLB, "deployments", "metallb-operator-controller-manager")
-		if strings.Contains(output, "Error from server") {
-			t.Errorf("failed to get metallb-operator-controller-manager: %s", output)
-		}
+		oc.Get(t, ns.MetalLB, "deployments", "metallb-operator-controller-manager", assert.OutputDoesNotContain(
+			"Error from server",
+			"metallb-operator-controller-manager was found as expected",
+			"failed to get metallb-operator-controller-manager"))
 	})
 	oc.WaitCondition(t, ns.MetalLB, "deployments", "metallb-operator-controller-manager", "Available")
 }
 
-func deployMetalLB(t test.TestHelper) {
+func deployMetalLB(t test.TestHelper, oc oc.OC) {
 	t.LogStep("Deploy MetalLB")
 	oc.ApplyString(t, ns.MetalLB, metallb)
 	retry.UntilSuccess(t, func(t test.TestHelper) {
-		output := oc.Get(t, ns.MetalLB, "deployments", "controller")
-		if strings.Contains(output, "Error from server") {
-			t.Errorf("failed to get metallb controller: %s", output)
-		}
+		oc.Get(t, ns.MetalLB, "deployments", "controller", assert.OutputDoesNotContain(
+			"Error from server",
+			"MetalLB controller was found",
+			"failed to get MetalLB controller"))
 	})
 	oc.WaitCondition(t, ns.MetalLB, "deployments", "controller", "Available")
 }
 
-func createAddressPool(t test.TestHelper) {
+func createAddressPool(t test.TestHelper, oc oc.OC) {
 	t.LogStep("Fetch worker internal IPs")
+	ipAddrPool := `
+apiVersion: metallb.io/v1beta1
+kind: IPAddressPool
+metadata:
+  name: worker-internal-ips
+  namespace: metallb-system
+spec:
+  addresses:
+`
 	var ips []string
 	retry.UntilSuccess(t, func(t test.TestHelper) {
 		// This command fails in zsh, so it may not work on some local environments, therefore it's wrapped in bash -c
-		out := shell.Execute(t, `bash -c 'kubectl get nodes -l node-role.kubernetes.io/worker -o jsonpath={.items[*].status.addresses[?\(@.type==\"InternalIP\"\)].address}'`)
-		if strings.Contains(out, "Error from server") {
-			t.Errorf("failed to get worker IPs: %s", out)
-		}
+		out := shell.Execute(t,
+			`bash -c 'kubectl get nodes -l node-role.kubernetes.io/worker -o jsonpath={.items[*].status.addresses[?\(@.type==\"InternalIP\"\)].address}'`,
+			assert.OutputDoesNotContain("Error from server", "Found internal IPs", "failed to get internal node IPs"))
 		ips = strings.Fields(out)
 		for _, rawIP := range ips {
 			if ip := net.ParseIP(rawIP); ip == nil {
 				t.Errorf("failed to parse fetched IPs: %s", ips)
 			}
+			ipAddrPool += fmt.Sprintf("  - %[1]s-%[1]s\n", rawIP)
 		}
 	})
-	t.LogStep("Create IP address pool for MetalLB")
-	oc.ApplyTemplate(t, ns.MetalLB, ipAddressPoolTmpl, map[string]interface{}{"IPs": ips})
+	t.LogStepf("Create IPAddressPool for MetalLB: %s", ipAddrPool)
+	oc.ApplyString(t, ns.MetalLB, ipAddrPool)
 }
--- pkg/util/shell/shell_helper.go
+++ pkg/util/shell/shell_helper.go
@@ -34,7 +34,7 @@ func ExecuteWithEnvAndInput(t test.TestHelper, env []string, cmd string, input s
 	t.T().Helper()
 	output, err := execShellCommand(cmd, env, input)
 	if err != nil {
-		t.Fatalf("Command failed: %s\n%serror: %s", cmd, appendNewLine(output), err)
+		t.Logf("Command failed: %s\n%serror: %s", cmd, appendNewLine(output), err)
 	}
 	for _, check := range checks {
 		check(t, output)
