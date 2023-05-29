7fc8660179a4ca3f8ebd8eaf354e21c25d973ed8
Francisco Herrera Lira <fjglira@gmail.com>
2023-05-29 17:27:05 +0200

    OSSM-3998 Fix OCP Parse for Certmanager (#561)
    * fix test case (#554)
    
    Co-authored-by: Francisco Herrera <frherrer@redhat.com>
    
    * OSSM-3962: Add test for cacerts secret provided by cert-manager (#555)
    
    * OSSM-3962: Add e2d test for cacerts provided by cert-manager
    
    Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>
    
    * OSSM-3962: Move cert-manager test to a dedicated package
    
    Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>
    
    * Add support for Cleanup function to setupTestHelper
    
    Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>
    
    * Implement test for cacerts provided by cert-manager
    
    Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>
    
    * Create certificates and issuers without additional retries
    
    Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>
    
    ---------
    
    Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>
    
    * Fix nil pointer in setupTestHelper.Run() (#557)
    
    Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>
    
    * OSSM-3967 Fix Cert manager test cases (#559)
    
    * Adding skip for test certmanager
    
    * Adding skip to setup helper
    
    * minor change
    
    * fix the skip part
    
    ---------
    
    Co-authored-by: Francisco Herrera <frherrer@redhat.com>
    
    * fix ocp parse
    
    * changes after review
    
    * Making changes from review
    
    * simplify func
    
    ---------
    
    Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>
    Co-authored-by: Francisco Herrera <frherrer@redhat.com>
    Co-authored-by: Jacek Ewertowski <jewertow@redhat.com>

---
+++ pkg/util/version/ocp_version.go
@@ -0,0 +1,10 @@
+package version
+
+var (
+	OCP_4_9  = ParseVersion("4.9.0")
+	OCP_4_10 = ParseVersion("4.10.0")
+	OCP_4_11 = ParseVersion("4.11.0")
+	OCP_4_12 = ParseVersion("4.12.0")
+	OCP_4_13 = ParseVersion("4.13.0")
+	OCP_4_14 = ParseVersion("4.14.0")
+)
--- pkg/util/version/version.go
+++ pkg/util/version/version.go
@@ -6,74 +6,33 @@ import (
 	"strings"
 )
 
-var (
-	SMCP_2_0 = ParseVersion("v2.0")
-	SMCP_2_1 = ParseVersion("v2.1")
-	SMCP_2_2 = ParseVersion("v2.2")
-	SMCP_2_3 = ParseVersion("v2.3")
-	SMCP_2_4 = ParseVersion("v2.4")
-
-	OCP_4_9  = ParseOCPVersion("4.9.0")
-	OCP_4_10 = ParseOCPVersion("4.10.0")
-	OCP_4_11 = ParseOCPVersion("4.11.0")
-	OCP_4_12 = ParseOCPVersion("4.12.0")
-	OCP_4_13 = ParseOCPVersion("4.13.0")
-	OCP_4_14 = ParseOCPVersion("4.14.0")
-
-	VERSIONS = []*Version{
-		&SMCP_2_0,
-		&SMCP_2_1,
-		&SMCP_2_2,
-		&SMCP_2_3,
-		&SMCP_2_4,
-		&OCP_4_9,
-		&OCP_4_10,
-		&OCP_4_11,
-		&OCP_4_12,
-		&OCP_4_13,
-		&OCP_4_14,
-	}
-)
-
 type Version struct {
 	Major int
 	Minor int
 }
 
 func ParseVersion(version string) Version {
+	// This func can handle smcp versions and ocp versions. Example: smcp version v2.1.0 and ocp version 4.10.0
 	if len(version) > 0 && version[0] == 'v' {
 		version = version[1:]
 	}
+
 	majorMinor := strings.Split(version, ".")
-	if len(majorMinor) != 2 {
-		panic(fmt.Sprintf("invalid SMCP version: %s", version))
+
+	if len(majorMinor) < 2 {
+		panic(fmt.Sprintf("invalid version: %s", version))
 	}
+
 	major, err := strconv.Atoi(majorMinor[0])
 	if err != nil {
-		panic(fmt.Sprintf("invalid SMCP version: %s", version))
+		panic(fmt.Sprintf("invalid version: %s", version))
 	}
+
 	minor, err := strconv.Atoi(majorMinor[1])
 	if err != nil {
-		panic(fmt.Sprintf("invalid SMCP version: %s", version))
+		panic(fmt.Sprintf("invalid version: %s", version))
 	}
 
-	return Version{Major: major, Minor: minor}
-}
-
-func ParseOCPVersion(version string) Version {
-	// OCP version is in the form of "major.minor.patch", example: 4.10.59
-	majorMinorPatch := strings.Split(version, ".")
-	if len(majorMinorPatch) != 3 {
-		panic(fmt.Sprintf("invalid OCP version: %s", version))
-	}
-	major, err := strconv.Atoi(majorMinorPatch[0])
-	if err != nil {
-		panic(fmt.Sprintf("invalid OCP version: %s", version))
-	}
-	minor, err := strconv.Atoi(majorMinorPatch[1])
-	if err != nil {
-		panic(fmt.Sprintf("invalid OCP version: %s", version))
-	}
 	return Version{Major: major, Minor: minor}
 }
 
@@ -105,27 +64,4 @@ func (this Version) LessThanOrEqual(that Version) bool {
 
 func (this Version) String() string {
 	return fmt.Sprintf("v%d.%d", this.Major, this.Minor)
-}
-
-func (this Version) GetPreviousVersion() Version {
-	var prevVersion *Version
-	for _, v := range VERSIONS {
-		if *v == this {
-			if prevVersion == nil {
-				panic(fmt.Sprintf("version %s is the first supported version", this))
-			}
-			return *prevVersion
-		}
-		prevVersion = v
-	}
-	panic(fmt.Sprintf("version %s not found in VERSIONS", this))
-}
-
-func (this Version) IsSupported() bool {
-	for _, v := range VERSIONS {
-		if *v == this {
-			return true
-		}
-	}
-	return false
 }
