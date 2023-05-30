8f59db6df9c59cfd4d25f34d8d922b3a4f662f39
Jacek Ewertowski <jewertow@redhat.com>
2023-05-30 11:27:09 +0200

    Revert changes in federation tests
    Signed-off-by: Jacek Ewertowski <jewertow@redhat.com>

--- pkg/tests/ossm-federation/main_test.go
+++ pkg/tests/ossm-federation/main_test.go
@@ -3,17 +3,10 @@ package ossm_federation
 import (
 	"testing"
 
-	"github.com/maistra/maistra-test-tool/pkg/metallb"
-	"github.com/maistra/maistra-test-tool/pkg/util/env"
 	"github.com/maistra/maistra-test-tool/pkg/util/test"
 )
 
 func TestMain(m *testing.M) {
 	test.NewSuite(m).
-		Setup(func(t test.TestHelper) {
-			if env.IsMetalLBInternalIPEnabled() {
-				metallb.InstallIfNotExist(t, env.GetKubeconfig(), env.GetKubeconfig2())
-			}
-		}).
 		Run()
 }
