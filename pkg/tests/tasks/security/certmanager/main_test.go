3d2e68f8e061aecc4115b97b17367038e9d49da2
Francisco Herrera <frherrer@redhat.com>
2023-06-23 13:47:40 +0200

    fix cert manager test case

--- pkg/tests/tasks/security/certmanager/main_test.go
+++ pkg/tests/tasks/security/certmanager/main_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/maistra/maistra-test-tool/pkg/util/env"
 	"github.com/maistra/maistra-test-tool/pkg/util/oc"
 	"github.com/maistra/maistra-test-tool/pkg/util/pod"
+	"github.com/maistra/maistra-test-tool/pkg/util/retry"
 	"github.com/maistra/maistra-test-tool/pkg/util/test"
 	"github.com/maistra/maistra-test-tool/pkg/util/version"
 )
@@ -72,8 +73,10 @@ func setupCertManagerOperator(t test.TestHelper) {
 
 	t.LogStep("Install cert-manager-operator")
 	oc.ApplyString(t, certManagerOperatorNs, certManagerOperator)
-	oc.WaitPodReady(t, pod.MatchingSelector("name=cert-manager-operator", certManagerOperatorNs))
-	oc.WaitPodReady(t, pod.MatchingSelector("app=cert-manager", certManagerNs))
+	retry.UntilSuccess(t, func(t test.TestHelper) {
+		oc.WaitPodReady(t, pod.MatchingSelector("name=cert-manager-operator", certManagerOperatorNs))
+		oc.WaitPodReady(t, pod.MatchingSelector("app=cert-manager", certManagerNs))
+	})
 
 	t.LogStep("Create root ca")
 	oc.ApplyString(t, certManagerNs, rootCA)
