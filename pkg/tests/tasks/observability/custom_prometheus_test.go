3f09e6431fee20090cdfaf5606512dfc686bdb01
Francisco Herrera <frherrer@redhat.com>
2023-06-30 11:56:02 +0200

    skip custome prometheus test case on version older than 4.11

--- pkg/tests/tasks/observability/custom_prometheus_test.go
+++ pkg/tests/tasks/observability/custom_prometheus_test.go
@@ -28,6 +28,10 @@ func TestCustomPrometheus(t *testing.T) {
 		if smcpVer.LessThan(version.SMCP_2_4) {
 			t.Skip("Extension providers are not supported in OSSM older than v2.4.0")
 		}
+		ocpVersion := version.ParseVersion(oc.GetOCPVersion(t))
+		if ocpVersion.LessThan(version.OCP_4_11) {
+			t.Skip("Custom Prometheus operator from red hat is not supported in OCP older than v4.11")
+		}
 
 		t.Cleanup(func() {
 			oc.RecreateNamespace(t, ns.Bookinfo)
