32ff33d13b271ab234fe39e078e6ce02f24a2358
Dean Doyle <deandoyle1502@gmail.com>
2024-08-29 11:12:02 +0100

    Added comparison test

--- pkg/tests/ossm/operator/version_test.go
+++ pkg/tests/ossm/operator/version_test.go
@@ -1,22 +1,41 @@
 package operator
 
+import (
+	"os/exec"
+	"strings"
+	"testing"
+
+	"github.com/maistra/maistra-test-tool/pkg/util/env"
+	"github.com/maistra/maistra-test-tool/pkg/util/pod"
+	"github.com/maistra/maistra-test-tool/pkg/util/test"
+)
+
+const EXPECTED_VERSION = "v2.5" // Replace with the expected version
+
 func TestVersion(t *testing.T) {
-	NewTest(t).Run(func(t TestHelper) {
+	test.NewTest(t).Run(func(t test.TestHelper) {
 		t.Log("Test to verify helm chart version matches expected version")
 
 		operatorPod := pod.MatchingSelector("name=istio-operator", env.GetOperatorNamespace())
-		
-		cmd := exec.Command("kubectl exec deploy/istio-operator -n " + env.GetOperatorNamespace() +
-			" -- cat /usr/local/share/istio-operator/helm/" + env.GetSMCPVersion() +
-			"/istio-control/istio-discovery/templates/deployment.yaml | grep maistra-version | awk '{print $2}'")
-		
-			outputBytes, err := cmd.Output()
-		if err != nil{
-			t.Fatalf("Failed to execture command: %v", err)
+		if operatorPod == nil {
+			t.Fatalf("Failed to find istio-operator pod in namespace %s", env.GetOperatorNamespace())
 		}
 
-		output := string(outputBytes)
-		
-	},
-			
-		//kubectl exec deploy/istio-operator -- cat /usr/local/share/istio-operator/helm/v2.5/istio-control/istio-discovery/templates/deployment.yaml | grep maistra-version
+		cmd := exec.Command("kubectl", "exec", "deploy/istio-operator", "-n", env.GetOperatorNamespace(),
+			"--", "cat", "/usr/local/share/istio-operator/helm/", env.GetSMCPVersion().String(),
+			"/istio-control/istio-discovery/templates/deployment.yaml", "|", "grep", "maistra-version", "|", "awk", "'{print $2}'")
+
+		outputBytes, err := cmd.Output()
+		if err != nil {
+			t.Fatalf("Failed to execute command: %v", err)
+		}
+
+		output := strings.TrimSpace(string(outputBytes))
+
+		if output != EXPECTED_VERSION {
+			t.Fatalf("Version mismatch: expected %s, got %s", EXPECTED_VERSION, output)
+		}
+
+		t.Logf("Version matches expected version: %s", EXPECTED_VERSION)
+	})
+}
